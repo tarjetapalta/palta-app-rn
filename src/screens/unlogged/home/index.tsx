5c434146f8029d091c95876bdf045f9d24c433dd
Pablo Araujo <pabloaraujo@MacBook-Pro-de-Pablo.local>
2021-03-29 15:14:00 -0300

    feature basic config with husky

--- src/screens/unlogged/home/index.tsx
+++ src/screens/unlogged/home/index.tsx
@@ -1,17 +1,8 @@
 import React, { FunctionComponent } from 'react';
 import { View, Text } from 'react-native';
 
-export const Home: FunctionComponent = () => {
-  console.log('HOME');
-
-  return (
-    <View
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-      }}>
-      <Text style={{ color: 'coral' }}>HOME SCREEN</Text>
-    </View>
-  );
-};
+export const Home: FunctionComponent = () => (
+  <View>
+    <Text>HOME SCREEN</Text>
+  </View>
+);
